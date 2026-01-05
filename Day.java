78d489106a6fdd8f75911c1f36eb63b089a7b961
the-sanket-khedkar <sanketkhedkar12@gmail.com>
2026-01-05 14:13:05 +0530

    Initial commit: Added DSA notes and codes

---
+++ Day.java
@@ -0,0 +1,17 @@
+class Day{
+    public static void main(String[] args){
+        int arr[] = {1,2,2,3,7,5,8} ;
+        System.out.println(arr[0]);
+        System.out.println(arr[5]);
+        //int Scanner = new Scanner()
+        int n = 5;
+        int[] newArr = new int[n];
+        newArr[0] = 10;
+        System.out.println();
+        for (int i = 0; i < n; i++){
+            
+            System.out.println(newArr[i]);
+        }
+
+    }
+}
